99c6d69cf4a01a25f8334f4df790616b33e6f5b0
kosuke-10 <kousuke177@icloud.com>
2024-11-25 22:01:30 +0900

    ユーザのIDの型変更

--- backend/app/models/model.py
+++ backend/app/models/model.py
@@ -13,15 +13,15 @@ class Question(db.Model):
     """質問ID"""
     message: str = db.Column(db.String(255), nullable=False)
     """質問内容"""
-    user_id: int = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id: str = db.Column(db.String(255), db.ForeignKey('users.id'), nullable=False)
     """ユーザーID"""
     status_id: int = db.Column(db.Integer, db.ForeignKey('statuses.id'), nullable=False)
     """ステータスID"""
     mode_id: int = db.Column(db.Integer, db.ForeignKey('modes.id'), nullable=False)
     """モードID"""
-    updated_at: str = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
     """更新日時"""
-    created_at: str = db.Column(db.DateTime, default=db.func.now(), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
     """作成日時"""
 
     user: Mapped['User'] = db.relationship('User', back_populates='questions')
@@ -39,6 +39,31 @@ class Question(db.Model):
             'updated_at': self.updated_at,
             'created_at': self.created_at
         }
+
+class User(db.Model):
+    """
+    ユーザークラス
+    """
+    __tablename__ = 'users'
+
+    id: str = db.Column(db.String(255), primary_key=True)  # GoogleのIDを保存
+    """ユーザーID"""
+    email: str = db.Column(db.String(255), unique=True, nullable=False)
+    """メールアドレス"""
+    name: str = db.Column(db.String(100), nullable=False)
+    """名前"""
+
+    questions: Mapped[list['Question']] = db.relationship('Question', back_populates='user')
+
+    def to_dict(self) -> dict:
+        return {
+            'id': self.id,
+            'email': self.email,
+            'name': self.name
+        }
+
+
+
 
 
 class Mode(db.Model):
@@ -58,83 +83,6 @@ class Mode(db.Model):
         return {
             'id': self.id,
             'name': self.name
-        }
-
-
-class Answer(db.Model):
-    """
-    回答クラス
-    """
-    __tablename__ = 'answers'
-
-    id: int = db.Column(db.Integer, primary_key=True)
-    """回答ID"""
-    question_id: str = db.Column(CHAR(36), db.ForeignKey('questions.id'), unique=True, nullable=False)
-    """質問ID"""
-    message: str = db.Column(db.String(255), nullable=False)
-    """回答内容"""
-    created_at: str = db.Column(db.DateTime, default=db.func.now(), nullable=False)
-    """作成日時"""
-
-    question: Mapped['Question'] = db.relationship('Question', back_populates='answers')
-    related_words: Mapped[list['RelatedWord']] = db.relationship('RelatedWord', back_populates='answer')
-    references: Mapped[list['Reference']] = db.relationship('Reference', back_populates='answer')
-
-    def to_dict(self) -> dict:
-        return {
-            'id': self.id,
-            'question_id': self.question_id,
-            'message': self.message,
-            'created_at': self.created_at
-        }
-
-
-class RelatedWord(db.Model):
-    """
-    関連語クラス
-    """
-    __tablename__ = 'related_words'
-
-    id: int = db.Column(db.Integer, primary_key=True)
-    """関連語ID"""
-    answer_id: int = db.Column(db.Integer, db.ForeignKey('answers.id'), nullable=False)
-    """回答ID"""
-    related_word: str = db.Column(db.String(255), nullable=False)
-    """関連語"""
-
-    answer: Mapped['Answer'] = db.relationship('Answer', back_populates='related_words')
-
-    def to_dict(self) -> dict:
-        return {
-            'id': self.id,
-            'answer_id': self.answer_id,
-            'related_word': self.related_word
-        }
-
-
-class Reference(db.Model):
-    """
-    参考記事クラス
-    """
-    __tablename__ = 'references'
-
-    id: int = db.Column(db.Integer, primary_key=True)
-    """参考記事リンクID"""
-    answer_id: int = db.Column(db.Integer, db.ForeignKey('answers.id'), nullable=False)
-    """回答ID"""
-    title: str = db.Column(db.String(255), nullable=False)
-    """記事名"""
-    url: str = db.Column(db.String(2048), nullable=False)
-    """記事URL"""
-
-    answer: Mapped['Answer'] = db.relationship('Answer', back_populates='references')
-
-    def to_dict(self) -> dict:
-        return {
-            'id': self.id,
-            'answer_id': self.answer_id,
-            'title': self.title,
-            'url': self.url
         }
 
 
@@ -158,24 +106,27 @@ class Status(db.Model):
         }
 
 
-class User(db.Model):
+class Answer(db.Model):
     """
-    ユーザークラス
+    回答クラス
     """
-    __tablename__ = 'users'
+    __tablename__ = 'answers'
 
     id: int = db.Column(db.Integer, primary_key=True)
-    """ユーザーID"""
-    email: str = db.Column(db.String(255), unique=True, nullable=False)
-    """メールアドレス"""
-    name: str = db.Column(db.String(100), nullable=False)
-    """名前"""
+    """回答ID"""
+    question_id: str = db.Column(CHAR(36), db.ForeignKey('questions.id'), unique=True, nullable=False)
+    """質問ID"""
+    message: str = db.Column(db.String(255), nullable=False)
+    """回答内容"""
+    created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
+    """作成日時"""
 
-    questions: Mapped[list['Question']] = db.relationship('Question', back_populates='user')
+    question: Mapped['Question'] = db.relationship('Question', back_populates='answers')
 
     def to_dict(self) -> dict:
         return {
             'id': self.id,
-            'email': self.email,
-            'name': self.name
+            'question_id': self.question_id,
+            'message': self.message,
+            'created_at': self.created_at
         }
